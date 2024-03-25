43b6a3b2e490fdd0d01948adbddd72d62f9c3afc
Marc Paquette <5941665+MarkzP@users.noreply.github.com>
2024-03-25 16:23:27 -0400

    Back to basics

--- README.md
+++ README.md
@@ -5,13 +5,15 @@
 ## Dependencies:
 
  Hardware:
- - Hacked TonePort UX1 (codec connected directly to Teensy I2S)
- - 4 pushbuttons connected to pins 2,3,4,5 to select channel
+ - Teensy 4.0/4.1
+ - Teensy Guitar Audio Shield http://blackaddr.com/products/
+ - 1 pot connected on A0 to select between Dist, Edge, Cln1 & Cln2
 
 Libraries:
 
+ - BALibrary: https://github.com/Blackaddr/BALibrary
  - Teensy OpenAudio Library https://github.com/chipaudette/OpenAudio_ArduinoLibrary
  - AudioEffectDynamics_F32 https://github.com/MarkzP/AudioEffectDynamics_F32
- - ModulatedDelay_F32 https://github.com/MarkzP/ModulatedDelay_F32
- - Freeverb_F32 https://github.com/MarkzP/Freeverb_F32
- - NonLinear_F32 https://github.com/MarkzP/NonLinear_F32
+ - Modulated Delay https://github.com/MarkzP/ModulatedDelay_F32
+ - Freeverb https://github.com/MarkzP/Freeverb_F32
+ - NonLinear https://github.com/MarkzP/NonLinear_F32
--- X100.ino
+++ X100.ino
@@ -1,7 +1,19 @@
-#include "effect_dynamics_F32.h"              // https://github.com/MarkzP/AudioEffectDynamics_F32
-#include "effect_modulated_delay_F32.h"       // https://github.com/MarkzP/ModulatedDelay_F32
-#include "effect_freeverb_F32.h"              // https://github.com/MarkzP/Freeverb_F32
-#include "effect_nonlinear_F32.h"             // https://github.com/MarkzP/NonLinear_F32
+
+
+#include "BALibrary.h"
+using namespace BALibrary;
+
+#include "effect_dynamics_F32.h"
+#include "effect_modulated_delay_F32.h"
+#include "effect_freeverb_F32.h"
+#include "effect_nonlinear_F32.h"
+
+// AudioEffectDynamics_F32
+// AudioEffectFreeverbStereo_F32
+// AudioEffectModulatedDelay_F32
+// AudioEffectNonLinear_F32
+// AudioEffectDelay_OA_F32
+
 #include "OpenAudio_ArduinoLibrary.h"
 #include "AudioStream_F32.h"
 #include <Audio.h>
@@ -11,54 +23,44 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S_F32        audioIn;        //xy=60,40
-AudioFilterBiquad_F32    preEq;          //xy=161,138
-AudioMixer4_F32          inputMixer;     //xy=195,53
-AudioEffectDynamics_F32  dynamics;       //xy=293,144
-AudioAnalyzePeak_F32     inputLevel;     //xy=383,55
-AudioEffectNonLinear_F32 distortion;     //xy=429,144
-AudioSynthWaveform_F32   chorusLfo;      //xy=556,58
-AudioFilterEqualizer_F32 postEq;         //xy=557,144
-AudioMixer4_F32          delayMixer;     //xy=727,283
-AudioEffectModulatedDelay_F32 chorusModDelay; //xy=738,106
-AudioFilterBiquad_F32    chorusPostFilter; //xy=917,106
-AudioEffectDelay_OA_F32  reverbPreDelay; //xy=923,239
-AudioFilterBiquad_F32    delayFilter;    //xy=929,326
-AudioEffectFreeverbStereo_F32 reverb;         //xy=1096,240
-AudioEffectDelay_OA_F32  delayLine;      //xy=1097,327
-AudioEffectDelay_OA_F32  chorusPostDelay; //xy=1105,106
-AudioMixer4_F32          mixerL;         //xy=1297,194
-AudioMixer4_F32          mixerR;         //xy=1299,334
-AudioOutputI2S_F32       audioOut;       //xy=1439,231
-AudioConnection_F32          patchCord1(audioIn, 0, inputMixer, 0);
-AudioConnection_F32          patchCord2(audioIn, 1, inputMixer, 1);
-AudioConnection_F32          patchCord3(preEq, 0, dynamics, 0);
-AudioConnection_F32          patchCord4(inputMixer, inputLevel);
-AudioConnection_F32          patchCord5(inputMixer, preEq);
-AudioConnection_F32          patchCord6(dynamics, distortion);
-AudioConnection_F32          patchCord7(distortion, postEq);
-AudioConnection_F32          patchCord8(chorusLfo, 0, chorusModDelay, 1);
-AudioConnection_F32          patchCord10(postEq, 0, mixerL, 0);
-AudioConnection_F32          patchCord11(postEq, 0, mixerR, 0);
-AudioConnection_F32          patchCord12(postEq, 0, delayMixer, 0);
-AudioConnection_F32          patchCord13(postEq, reverbPreDelay);
-AudioConnection_F32          patchCord14(postEq, 0, chorusModDelay, 0);
-AudioConnection_F32          patchCord15(delayMixer, delayFilter);
-AudioConnection_F32          patchCord16(chorusModDelay, chorusPostFilter);
-AudioConnection_F32          patchCord17(chorusPostFilter, chorusPostDelay);
-AudioConnection_F32          patchCord18(chorusPostFilter, 0, mixerL, 1);
-AudioConnection_F32          patchCord19(reverbPreDelay, reverb);
-AudioConnection_F32          patchCord20(delayFilter, delayLine);
-AudioConnection_F32          patchCord21(reverb, 0, mixerL, 2);
-AudioConnection_F32          patchCord22(reverb, 1, mixerR, 2);
-AudioConnection_F32          patchCord23(delayLine, 0, mixerR, 3);
-AudioConnection_F32          patchCord24(delayLine, 0, mixerL, 3);
-AudioConnection_F32          patchCord25(delayLine, 0, delayMixer, 1);
-AudioConnection_F32          patchCord26(chorusPostDelay, 0, mixerR, 1);
-AudioConnection_F32          patchCord27(mixerL, 0, audioOut, 0);
-AudioConnection_F32          patchCord28(mixerR, 0, audioOut, 1);
+AudioInputI2S_F32        audioIn;    //xy=81,49
+AudioFilterBiquad_F32    preEq;          //xy=85,173
+AudioEffectDynamics_F32 compressor;     //xy=223,173
+AudioEffectNonLinear_F32      distortion;          //xy=363,125
+AudioFilterBiquad_F32    EQ2;        //xy=492,174
+AudioFilterEqualizer_F32 EQ1; //xy=493,125
+AudioMixer4_F32          mixerMode;      //xy=646,155
+AudioSynthWaveform_F32   chorusMod;      //xy=805,105
+AudioFilterBiquad_F32    chorusPreFilter; //xy=809,50
+AudioFilterBiquad_F32    reverbPreFilter;        //xy=839,293
+AudioEffectDelay_OA_F32     chorusPostDelay;         //xy=982,240
+AudioEffectFreeverbStereo_F32      reverb;    //xy=989,293
+AudioEffectModulatedDelay_F32    chorusModDelay; //xy=993,56
+AudioMixer4_F32          mixerL;         //xy=1175,172
+AudioMixer4_F32          mixerR;         //xy=1175,247
+AudioOutputI2S_F32       audioOut;   //xy=1312,207
+AudioConnection_F32          patchCord1(audioIn, 0, preEq, 0);
+AudioConnection_F32          patchCord2(preEq, compressor);
+AudioConnection_F32          patchCord3(compressor, distortion);
+AudioConnection_F32          patchCord4(compressor, EQ2);
+AudioConnection_F32          patchCord5(distortion, EQ1);
+AudioConnection_F32          patchCord6(EQ2, 0, mixerMode, 2);
+AudioConnection_F32          patchCord7(EQ1, 0, mixerMode, 1);
+AudioConnection_F32          patchCord8(mixerMode, 0, mixerL, 0);
+AudioConnection_F32          patchCord9(mixerMode, 0, mixerR, 0);
+AudioConnection_F32          patchCord10(mixerMode, chorusPreFilter);
+AudioConnection_F32          patchCord11(mixerMode, reverbPreFilter);
+AudioConnection_F32          patchCord12(chorusMod, 0, chorusModDelay, 1);
+AudioConnection_F32          patchCord13(chorusPreFilter, 0, chorusModDelay, 0);
+AudioConnection_F32          patchCord14(reverbPreFilter, reverb);
+AudioConnection_F32          patchCord15(chorusPostDelay, 0, mixerR, 1);
+AudioConnection_F32          patchCord16(reverb, 0, mixerL, 2);
+AudioConnection_F32          patchCord17(reverb, 1, mixerR, 2);
+AudioConnection_F32          patchCord19(chorusModDelay, 0, mixerL, 1);
+AudioConnection_F32          patchCord20(chorusModDelay, 0, chorusPostDelay, 0);
+AudioConnection_F32          patchCord21(mixerL, 0, audioOut, 0);
+AudioConnection_F32          patchCord22(mixerR, 0, audioOut, 1);
 // GUItool: end automatically generated code
-
 
 
 typedef enum
@@ -71,157 +73,143 @@ typedef enum
   
 } X100_Channels;
 
-const int posteqnf = 7;
-float posteqf[]  = {  100.0f,      200.0f,      400.0f,      800.0f,      1600.0f,      3200.0f,      6400.0f };
-float eq1g[]     = {   -6.0f,       -3.0f,       -1.5f,       -9.0f,       -12.0f,         1.5f,        -9.0f };
-float eq2g[]     = {   -9.0f,       -6.0f,        0.5f,        0.0f,         0.0f,         4.5f,         6.0f };
-float posteqg[]  = {    0.0f,        0.0f,        0.0f,        0.0f,         0.0f,         0.0f,         0.0f };
-const int posteqnc = 33;
-float posteqc[200];
-float posteqsl = 60.0f;
+const int eq1n = 8;
+float eq1f[] = { 100.0f, 200.0f, 350.0f, 800.0f, 1500.0f, 3000.0f, 6000.0f, 12000.0f };
+float eq1g[] = {  -6.0f,  -3.0f,  -1.5f,  -9.0f,  -12.0f,    1.5f,  -9.0f,   -36.0f };
+float eq1c[200];
 
 const int chorusDelaySamples = 1024;
 float chorusDelayLine[chorusDelaySamples];
 
-float instrumentGain =   1.000f;
-float mikeGain = 0.000f;
-float chorusGain = 0.750f;
-float reverbGain = 0.018f;
-float delayGain = 0.175f;
+BAAudioControlWM8731 codec;
+// regArray[WM8731_REG_INTERFACE] = 0b00001010;
+// write(WM8731_REG_INTERFACE, regArray[WM8731_REG_INTERFACE]); // I2S, 24 bit, MCLK slave
 
+
+BAPhysicalControls controls(0, 3, 0, 0);
+
+float chorusGain = 0.800f;
+float reverbGain = 0.014f;
+float mainGain =   1.000f;
 X100_Channels channel = X100_mute;
 
-void setInstrument(bool enable)
-{
-  mixerR.gain(0, enable ? instrumentGain : 0.0f);
-  mixerL.gain(0, enable ? instrumentGain : 0.0f);
-}
-
-void setChorus(bool enable)
-{
-  if (enable) {
-    chorusLfo.begin(0.032f, 1.7f, WAVEFORM_TRIANGLE);
-    chorusModDelay.begin(chorusDelayLine, chorusDelaySamples);
-    chorusPostFilter.setNotch(0, 6500.0f, 0.7f);
-    chorusPostFilter.setLowpass(1, 8000.0f, 0.5f);
-    chorusPostFilter.begin();
-    chorusPostDelay.delay(0, 17.0f);
-  }
-
-  mixerR.gain(1, enable ? -chorusGain : 0.0f);
-  mixerL.gain(1, enable ? chorusGain : 0.0f);
-}
-
-void setReverb(bool enable)
-{
-  if (enable) {
-    reverb.roomsize(0.9f);
-    reverb.damping(0.001f);
-    reverbPreDelay.delay(0, 15.0f);
-  }
-  
-  mixerR.gain(2, enable ? reverbGain : 0.0f);
-  mixerL.gain(2, enable ? reverbGain : 0.0f);
-}
-
-void setDelay(bool enable)
-{
-  if (enable) {
-    delayMixer.gain(0, 0.7f);
-    delayMixer.gain(1, 0.3f);
-    delayLine.delay(0, 300);
-    delayFilter.setHighpass(0, 500.0f, 0.7071f);
-    delayFilter.setLowpass(1, 3000.0f, 0.7071f);
-    delayFilter.begin();
-  }
-  
-  mixerR.gain(3, enable ? delayGain : 0.0f);
-  mixerL.gain(3, enable ? -delayGain : 0.0f);
-}
 
 void setChannel(X100_Channels ch)
 {
   if (ch == channel) return;
-
-  inputMixer.gain(0, 0.0f);
-  inputMixer.gain(1, 0.0f);
-  delay(20);
-
-  preEq.setHighpass(0, 200.0f, 0.7071f);
-  preEq.setLowShelf(1, 1000.0f, -18.0f, 0.2f);
-  preEq.setLowpass(2, 2400.0f, 1.7f);
-  preEq.begin();
-
-  dynamics.detector(AudioEffectDynamics_F32::DetectorType_DiodeBridge, 0.002f, 0.0f);
-  dynamics.gate(-75.0f, 0.0f, 3.0f, 6.0f, -14.0f);
-  dynamics.compression(-55.0f, 0.0015f, 2.5f, 4.5f);
-
   
-  distortion.gain(ch == X100_Dist ?  25.0f
-                  : ch == X100_Edge ? 2.5f
-                                    : 1.0f,
-                  ch == X100_Dist ? 0.18f
-                  : ch == X100_Edge ? 0.32f
-                                  : 1.0f);
+  AudioNoInterrupts(); 
+  distortion.gain(ch == X100_Dist ? 400.0f
+                : ch == X100_Edge ? 100.0f
+                : 1.0f);
+  mixerMode.gain(1, ch == X100_Dist ? 0.05f
+                  : ch == X100_Edge ? 0.08f
+                  : ch == X100_Cln1 ? 1.20f
+                  : 0.0f);
+  mixerMode.gain(2, ch == X100_Cln2 ? 1.2f : 0.0f);
+  AudioInterrupts();
 
-  distortion.curve(ch == X100_Dist ? 3.5f
-                :  ch == X100_Edge ? 3.8f
-                :  ch == X100_Cln1 ? 1.0f
-                :  0.0f);
-        
-  postEq.equalizerNew(posteqnf, &posteqf[0], ch == X100_Cln2 ? &eq2g[0] : &eq1g[0], posteqnc, &posteqc[0], posteqsl);     
-  
-  delay(20);
-  inputMixer.gain(0, ch == X100_mute ? 0.0f : -1.0f);
-  inputMixer.gain(1, ch == X100_mute ? 0.0f : 1.0f);
+  Serial.print("Channel "); Serial.println(ch);
   channel = ch;
-}
-
-float inputLevelDb = 0.0f;
-float inputLevelAvg = 0.0f;
-elapsedMillis reportInput;
-
-
-inline float unit2db(float u)
-{
-  if (u < 5.001554864521944e-7f) return -126.0f;
-  union { float f; uint32_t i; } vx = { u };
-  float y = vx.i;
-  y *= 1.1920928955078125e-7f;
-  y -= 126.94269504f;
-
-  return 6.02f * y;
 }
 
 void setup()
 {
+  TGA_PRO_MKII_REV1();
+
   AudioMemory(5);
-  AudioMemory_F32(192);
+  AudioMemory_F32(50);
   
-  setChorus(true);
-  setReverb(true);
-  setDelay(false);
-  setInstrument(true);
-  HW_Setup();
-  initSercom();
-  setChannel(X100_Cln1);
+  //preEq.setHighpass(0, 200.0f, 0.7071f);
+  preEq.setLowShelf(1, 1000.0f, -12.0f, 0.2f);
+  preEq.setLowpass(2, 2500.0f, 1.5f);
+  preEq.begin();
+
+  compressor.gate(-75.0f, 0.0f, 5.0f, 6.0f, -12.0f);
+  compressor.compression(-55.0f, 0.0015f, 2.5f, 3.5f, 1.5f);
+
+  EQ1.equalizerNew(eq1n, &eq1f[0], &eq1g[0], 30, &eq1c[0], 60.0f);
+
+  EQ2.setLowShelf(0, 300.0f, -9.0f, 0.5f);
+  EQ2.setHighShelf(1, 3500.0f, 6.0f, 0.5f);
+  EQ2.setLowpass(2, 11000.0f, 0.5f);
+  EQ2.begin();
+
+  if (!distortion.begin())
+  {
+    Serial.println("Not!");
+  }
+  distortion.bottom(0.4f);
+  distortion.tone(0.9f);
+
+  mixerMode.gain(0, 0.0f);
+  mixerMode.gain(1, 0.0f);
+  mixerMode.gain(2, 0.0f);
+  mixerMode.gain(3, 0.0f);
+
+  chorusPreFilter.setLowpass(1, 8000.0f, 0.7071f);
+  chorusPreFilter.begin();
+  chorusModDelay.begin(chorusDelayLine, chorusDelaySamples);
+  chorusMod.begin(0.032f, 1.7f, WAVEFORM_TRIANGLE);
+  chorusPostDelay.delay(0, 17);
+  mixerR.gain(1, -chorusGain);
+  mixerL.gain(1, chorusGain);
+
+  reverbPreFilter.setLowpass(1, 11000.0f, 0.3f);
+  reverbPreFilter.begin();
+  reverb.roomsize(0.8f);
+  reverb.damping(0.005f);
+  mixerR.gain(2, reverbGain);
+  mixerL.gain(2, reverbGain);
+ 
+  mixerR.gain(3, 0.0f);
+  mixerL.gain(3, 0.0f);
+  
+  delay(500); // allow the WM7831 extra time to power up
+  codec.enable();
+
+  codec.recalibrateDcOffset();
+  codec.setRightInMute(true);
+  codec.setLeftInputGain(31);
+
+  delay(500);
+
+  codec.setHeadphoneVolume(0.9f);
+
+  // Create the controls using the calib values
+  controls.addPot(BA_EXPAND_POT1_PIN, 0, 1023, false);
+  controls.addPot(BA_EXPAND_POT2_PIN, 0, 1023, false);
+  controls.addPot(BA_EXPAND_POT3_PIN, 0, 1023, false);
+
+  setChannel(X100_Dist);
 }
 
-
-void loop() {
-  if (inputLevel.available())
+void loop()
+{
+  while (Serial.available())
   {
-    float inputUnit = fabsf(inputLevel.read());
-    inputLevelDb = unit2db(inputUnit);    
-    inputLevelAvg += (inputLevelDb - inputLevelAvg) * 0.01f;
+    char c = Serial.read();
+    switch (c)
+    {
+      case '0': setChannel(X100_mute); break;
+      case '1': setChannel(X100_Dist); break;
+      case '2': setChannel(X100_Edge); break;
+      case '3': setChannel(X100_Cln1); break;
+      case '4': setChannel(X100_Cln2); break;
+    }
   }
 
-  if (reportInput > 50) {
-    reportInput = 0;
-    Serial.print("-126,0,");
-    Serial.println(inputLevelAvg, 5);
+  /*
+  float value;
+    
+  if (controls.checkPotValue(0, value))
+  {
+    if (value < 0.20) setChannel(X100_Cln2);
+    if (value > 0.25 && value < 0.45) setChannel(X100_Cln1);
+    if (value > 0.50 && value < 0.70) setChannel(X100_Edge);
+    if (value > 0.75) { setChannel(X100_Dist); }
   }
-  
-  HW_Loop();
-  handleSercom();
+  */
+
+  delay(20);
 }
